08df6e19470b5cdb370b21d12c8faa1742d45cef
AndresCampuzano <campuxano88@gmail.com>
2024-05-18 07:24:56 -0500

    Initial Commit

---
+++ SwiftExpenseTracker/SwiftExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExpenseTrackerApp.swift
+//  SwiftExpenseTracker
+//
+//  Created by Andres C G on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
